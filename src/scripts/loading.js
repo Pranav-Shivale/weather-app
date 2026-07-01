51a6588d81bd7f778cec98c04432c755c04a958f
Pranav-Shivale <pranavshivale69@gmail.com>
2026-07-01 21:11:22 +0530

    Add stop loading function

--- src/scripts/loading.js
+++ src/scripts/loading.js
@@ -1,8 +1,15 @@
 const loading = () => {
   const tempInfo = document.querySelector(".temp-info");
   const p = document.createElement("p");
+  p.classList.add("loading");
   p.textContent = "Fetching weather details...";
   tempInfo.append(p);
-}
+};
 
-export { loading };
+const stopLoading = () => {
+  const tempInfo = document.querySelector(".temp-info");
+  const p = document.querySelector(".loading");
+  tempInfo.removeChild(p);
+};
+
+export { loading, stopLoading };
